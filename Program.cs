495fdccda6036db34916b74c0b84ee4acf165ac7
Ivan MOLNAR <ivan.molnar@algosup.com>
2021-03-25 12:27:53 +0100

    hello

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World by algo!");
         }
     }
 }
